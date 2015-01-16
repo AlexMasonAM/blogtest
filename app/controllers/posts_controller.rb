0f74e17ea6373c38226141b653e51bfd52c3c0d3
AlexMasonAM <alexmmason@gmail.com>
2015-01-16 10:26:24 -0800

    created index views for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
+    @posts = Post.find_by(params[:id])
   end
   
   def new
@@ -11,10 +13,10 @@ class PostsController < ApplicationController
   end 
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body,))
+    @post = Post.new(params.require(:post).permit(:title, :body))
     
     if @post.save
-      #
+      redirect_to posts_path
     else
       render :new
     end
