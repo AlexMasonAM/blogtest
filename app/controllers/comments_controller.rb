ce86e9dd70d35fb95785923409fe7f668f21967c
AlexMasonAM <alexmmason@gmail.com>
2015-01-16 12:04:49 -0800

    updated Comments path to nested routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,10 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(params.require(:comment).permit(:body, :post_id))
-
-    if @comment.save
-      redirect_to post_path(@comment.post)
-    else
-      redirect_to post_path(@comment.post)
-    end
+    @comment = Comment.new(params.require(:comment).permit(:body))
+    @comment.post_id = params[:post_id]
+    @comment.save     
+    redirect_to post_path(@comment.post)
+    
   end
 end
