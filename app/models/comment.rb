bae895825bb6efb33cc44b58ed6148a835a1652a
AlexMasonAM <alexmmason@gmail.com>
2015-01-16 11:53:26 -0800

    added comments functionality

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,8 @@ class Comment
   belongs_to :post
 
   validates :body, presence: true
+
+  def date_published
+    created_at.localtime.strftime("%A, %B %-d, %Y at %l:%M %p")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-    resources :posts, except: [:edit, :update, :destroy]
+    resources :posts, except: [:edit, :update, :destroy] 
+    resources :comments,  only: [:create]
     
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
