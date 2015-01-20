aa32d3a8a4c0dcf39accbd04ffeef10fb6ae3226
AlexMasonAM <alexmmason@gmail.com>
2015-01-20 12:18:00 -0800

    updated commenter to Comments view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    render plain: params #shows all parameters
+    # render plain: params #shows all parameters for testing
 
     post = Post.find(params[:post_id])
-    comment = post.comments.create(params.require(:comment).permit(:body, commenter_attributes: [:name, :email]))
-    # redirect_to post_path(comment.post)
+    comment = post.comments.create(comment_params)
+    redirect_to post_path(comment.post)
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, commenter_attributes: [:name, :email])
   end
 
 end
